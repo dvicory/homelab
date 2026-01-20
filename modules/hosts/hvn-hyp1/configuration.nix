b2222f23757aee9d50ff863935b48988659ceb81
Daniel Vicory <dvicory@gmail.com>
2026-01-20 01:38:19 -0800

    Add some mounts and put in mergerfs

--- modules/hosts/hvn-hyp1/configuration.nix
+++ modules/hosts/hvn-hyp1/configuration.nix
@@ -44,7 +44,7 @@
   };
 
   flake.modules.nixos.hosts-hvn-hyp1 =
-    { config, ... }:
+    { config, pkgs, ... }:
     let
       contracts = config._contracts;
     in
@@ -95,6 +95,75 @@
 
         # Enable serial getty for fallback
         systemd.services."serial-getty@ttyS0".enable = true;
+
+        # Media filesystem
+        environment.systemPackages = [ pkgs.gocryptfs ];
+
+        fileSystems."/mnt/storage-crypt/media1" = {
+          device = "/dev/disk/by-label/media1";
+          fsType = "btrfs";
+          options = [ "noatime" ];
+        };
+
+        sops.secrets."hvn-hyp1/gocryptfs/media1" = {};
+
+        fileSystems."/mnt/storage-clear/media1" = {
+          device = "/mnt/storage-crypt/media1/crypt";
+          fsType = "fuse.gocryptfs";
+          options = [
+            "rw"
+            "allow_other"
+            "-passfile=${config.sops.secrets."hvn-hyp1/gocryptfs/media1".path}"
+          ];
+          depends = [ "/mnt/storage-crypt/media1" ];
+        };
+
+        fileSystems."/mnt/storage-crypt/media2" = {
+          device = "/dev/disk/by-label/media2";
+          fsType = "btrfs";
+          options = [ "noatime" ];
+        };
+
+        sops.secrets."hvn-hyp1/gocryptfs/media2" = {};
+
+        fileSystems."/mnt/storage-clear/media2" = {
+          device = "/mnt/storage-crypt/media2/crypt";
+          fsType = "fuse.gocryptfs";
+          options = [
+            "rw"
+            "allow_other"
+            "-passfile=${config.sops.secrets."hvn-hyp1/gocryptfs/media2".path}"
+          ];
+          depends = [ "/mnt/storage-crypt/media2" ];
+        };
+
+        fileSystems."/mnt/storage-crypt/media3" = {
+          device = "/dev/disk/by-label/media3";
+          fsType = "btrfs";
+          options = [ "noatime" ];
+        };
+
+        sops.secrets."hvn-hyp1/gocryptfs/media3" = {};
+
+        fileSystems."/mnt/storage-clear/media3" = {
+          device = "/mnt/storage-crypt/media3/crypt";
+          fsType = "fuse.gocryptfs";
+          options = [
+            "rw"
+            "allow_other"
+            "-passfile=${config.sops.secrets."hvn-hyp1/gocryptfs/media3".path}"
+          ];
+          depends = [ "/mnt/storage-crypt/media3" ];
+        };
+
+        dlab.storage.mergerfs."/mnt/storage/media" = {
+          branches = [
+            "/mnt/storage-clear/media1"
+            # "/mnt/storage-clear/media2"
+            "/mnt/storage-clear/media3"
+            # "/mnt/storage-clear/media-non-existent" # test non-existent branch
+          ];
+        };
       };
     };
 }
