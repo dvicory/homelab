311f51816b2139ef2e93ad6e986e96e658cbe39f
Daniel Vicory <dvicory@gmail.com>
2026-08-05 10:39:32 -0700

    chore(ssh): add key

--- modules/den/users/daniel.nix
+++ modules/den/users/daniel.nix
@@ -14,6 +14,10 @@
           tag = "mbp-2021";
           key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIItkbwb4903ks6RXq1AyRGRK3um1Wzo8tvo12lG9dete dvicory@mbp-2021-32gb";
         }
+        {
+          tag = "pixel-10-pro";
+          key = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHdw5FFx3Muwt472yeE63su7NeCkTlItBcnQtkx0oxEQjbf5EY+yLJajxieZyGSx5z4numb4ztIHj5voGT8RH5g=";
+        }
       ];
     };
   };
