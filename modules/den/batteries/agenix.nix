0047827d02675e3469c16d95656280f7763ab019
Daniel Vicory <dvicory@gmail.com>
2026-09-19 20:11:00 -0700

    fix(secrets): let agenix-rekey filter configurations

--- modules/den/batteries/agenix.nix
+++ modules/den/batteries/agenix.nix
@@ -99,10 +99,6 @@ in
     {
       agenix-rekey = {
         agePackage = pkgs.age;
-        nixosConfigurations = inputs.self.outputs.nixosConfigurations;
-        darwinConfigurations = inputs.self.outputs.darwinConfigurations;
-        collectHomeManagerConfigurations = true;
-        extraConfigurations = { };
       };
 
       devshells.default = {
